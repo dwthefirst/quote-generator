618f5a25664caa4d915d0728a79a73b596d2592a
ASUs <dwthefirst@gmail.com>
2023-07-05 16:26:22 -0700

    added testing comment

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//testing
+
 const quoteContainer = document.getElementById("quote-container");
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
